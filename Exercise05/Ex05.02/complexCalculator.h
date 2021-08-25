c0cce72c16591fda13e22ab1ca5d1fe6dc31855d
YitzchakMeltz <75029003+YitzchakMeltz@users.noreply.github.com>
2021-08-25 13:16:54 +0300

    add documentation

--- Exercise05/Ex05.02/complexCalculator.h
+++ Exercise05/Ex05.02/complexCalculator.h
@@ -26,22 +26,68 @@ typedef struct Number_t
     int isReal;
 }Number;
 
+/**
+* @brief runs the main calculator function
+*/
 int mainCalculator();
 
+/**
+* @brief makes the calculation given a mathematical operator
+* @param num1 a pointer to the first number
+* @param num2 a pointer to the second number
+* @param solution a pointer to the number where the solution will be saved
+* @param mathOp the math operator for the current math problem
+*/
 void calculate(Number *num1, Number *num2, Number *solution, char mathOp);
 
+/**
+* @brief adds two number together and places the solution in 'solution'
+* @param num1 a pointer to the first number
+* @param num2 a pointer to the second number
+* @param solution a pointer to the number where the solution will be saved
+*/
 void add(Number *num1, Number *num2, Number *solution);
 
+/**
+* @brief subtracts num2 from num1 and places the solution in 'solution'
+* @param num1 a pointer to the first number
+* @param num2 a pointer to the second number
+* @param solution a pointer to the number where the solution will be saved
+*/
 void sub(Number *num1, Number *num2, Number *solution);
 
+/**
+* @brief divides two number and places the solution in 'solution'
+* @param num1 a pointer to the first number
+* @param num2 a pointer to the second number
+* @param solution a pointer to the number where the solution will be saved
+*/
 void div(Number *num1, Number *num2, Number *solution);
 
+/**
+* @brief multiplies two number and places the solution in 'solution'
+* @param num1 a pointer to the first number
+* @param num2 a pointer to the second number
+* @param solution a pointer to the number where the solution will be saved
+*/
 void mult(Number *num1, Number *num2, Number *solution);
 
+/**
+* @brief inputs a number to num with user prompts
+* @param num pointer to a Number
+*/
 void numberInput(Number *num);
 
+/**
+* @brief inputs a number type to num with user prompts
+* @param num pointer to a Number
+*/
 void inputNumberType(Number *num);
 
+/**
+* @brief inputs a math operator with user prompts
+* @return the inputed math operator as a character
+*/
 char inputMathOp();
 
 #endif
