d1c4473fc75e58bc1f904f8388355ca0fc593482
YitzchakMeltz <75029003+YitzchakMeltz@users.noreply.github.com>
2021-08-25 14:29:46 +0300

    finish documentation

--- Exercise05/Ex05.02/complexCalculator.h
+++ Exercise05/Ex05.02/complexCalculator.h
@@ -8,18 +8,33 @@
 
 #include<stdbool.h>
 
+/**
+* @brief makes up a complex number
+* @param realPart the real part of the complex number
+* @param complexPart the complex part of the complex number
+*/
 typedef struct Complex_t
 {
     float realPart;
     float complexPart;
 }Complex;
 
+/**
+* @brief makes up a number type consisting of both complex number and real number types
+* @param complexNumber complex number
+* @param realNumber real number
+*/
 typedef union NumericType_t
 {
     Complex complexNumber;
     double realNumber;
 }NumberType;
 
+/**
+* @brief makes up a number type consisting of both a number and a flag
+* @param num a number of NumberType
+* @param isReal flag that is true if the number is real and false if the number is complex
+*/
 typedef struct Number_t
 {
     NumberType num;
