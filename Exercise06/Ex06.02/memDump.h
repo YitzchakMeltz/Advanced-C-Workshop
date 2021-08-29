2e8690842a553e29027930b7b0a1ab4db5f9dc16
YitzchakMeltz <75029003+YitzchakMeltz@users.noreply.github.com>
2021-08-29 23:19:08 +0300

    add documentation

--- Exercise06/Ex06.02/memDump.h
+++ Exercise06/Ex06.02/memDump.h
@@ -3,6 +3,16 @@
 
 #include<stdio.h>
 
+/**
+* @file Memory Dump Hexadecimal Function
+*/
+
+
+/**
+* @brief Prints the hexadecimal representation of that variable from the given address
+* @param address points to the address to print
+* @param addressSize contains the size of the data (bytes) stored at the memory address
+*/
 void PrintHex(void *address, size_t addressSize);
 
 #endif
